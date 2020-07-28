af1c435d5d631a82f96ece87b94eab012b74e414
rishika97 <61359053+rishika97@users.noreply.github.com>
2020-07-28 22:56:44 +0000

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/s_and_s/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.s_and_s
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Hello'),
+          centerTitle: true,
+        ),
+        body: Counter()
+      ),
+    );
+  }
+}
+
+class Counter extends StatefulWidget {
+  @override
+  _CounterState createState() => _CounterState();
+}
+class _CounterState extends State<Counter> {
+  int counter = 0;
+
+  void _increment() {
+    setState(() {
+      counter++;
+    });
+  }
+
+  void _decrement() {
+    setState(() {
+      counter--;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisSize: MainAxisSize.max,
+      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+      children: <Widget>[
+        RaisedButton(
+          onPressed: _increment,
+          child: Text('Increment'),
+          padding: EdgeInsets.all(20),
+        ),
+        RaisedButton(
+          onPressed: _decrement,
+          child: Text('Decrement'),
+          padding: EdgeInsets.all(20),
+        ),
+        Text('Count: $counter'),
+      ],
+    );
+  }
+}
